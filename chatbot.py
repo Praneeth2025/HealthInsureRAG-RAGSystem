6e2bfadfcecbb168ca760bbde44e0e61029ef9c2
MANGIPUDI VAMSI PRANEETH <153410995+Praneeth2025@users.noreply.github.com>
2025-06-23 17:53:44 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -31,7 +31,7 @@ from langchain.retrievers.contextual_compression import ContextualCompressionRet
 from langchain_core.tools import tool
 from langchain_community.tools.ddg_search.tool import DuckDuckGoSearchRun
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.agents import AgentExecutor, create_tool_calling_agent
+from langchain.agents import AgentExecutor
 
 # --- Caching ---
 from diskcache import Cache
